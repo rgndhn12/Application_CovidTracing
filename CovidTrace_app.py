5990c9481a7d8278c27775f1adf767f907be59f1
Rgn <reginefaemdahan@iskolarngbayan.pup.edu.ph>
2023-07-18 14:33:16 +0800

    add main label and button

--- CovidTrace_app.py
+++ CovidTrace_app.py
@@ -6,8 +6,20 @@
     #add entry
     #search entry
 
-import tkinter
-top = tkinter.Tk()
+import tkinter as tk
+
+#window
+top = tk.Tk()
+top.geometry("1000x1000")
 top.title (" COVID TRACING APP ")
+
+main_label = tk.Label(top, text= " HEALTH MATTERS ", font=("Avenir",25)) 
+main_label.pack(padx=30, pady=30)
+
+main_btn1 = tk.Button (top, text="Add Entry")
+main_btn1.pack(padx=100, pady=100)
+
+main_btn2 = tk.Button (top, text="Search Entry")
+main_btn2.pack(padx=300, pady=300)
 
 top.mainloop()
