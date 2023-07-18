959ab75a4161515a396bf50757eaa13bebdd27d1
Rgn <reginefaemdahan@iskolarngbayan.pup.edu.ph>
2023-07-18 15:46:30 +0800

    created file for another window

---
+++ Info_window.py
@@ -0,0 +1,13 @@
+# Dahan, Regine Fae M.      BSCPE 1-5    Covid Tracing App
+
+import tkinter as tk
+
+#window
+top = tk.Tk()
+top.geometry("1000x1000")
+top.title (" COVID TRACING APP ")
+
+frst_label = tk.Label(top, text= " Your Information... ", font=("Avenir",25)) 
+frst_label.pack(padx=30, pady=30)
+
+top.mainloop()
