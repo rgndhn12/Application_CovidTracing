877ca77429c38ffee8d57033a9188c190193ba95
Rgn <reginefaemdahan@iskolarngbayan.pup.edu.ph>
2023-07-18 14:01:45 +0800

    pseudocode

--- CovidTrace_app.py
+++ CovidTrace_app.py
@@ -1,1 +1,7 @@
 # Dahan, Regine Fae M.      BSCPE 1-5    Covid Tracing App
+
+#things included
+    #ask user for information
+    #write the colllected information in a file
+    #add entry
+    #search entry
