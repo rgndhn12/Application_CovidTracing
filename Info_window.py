4da4401f356846c48bf9ecb86e33b6b7c742632b
Rgn <reginefaemdahan@iskolarngbayan.pup.edu.ph>
2023-07-19 17:45:53 +0800

    added textbox

--- Info_window.py
+++ Info_window.py
@@ -12,22 +12,40 @@ frst_label = tk.Label(top_2, text= " YOUR INFORMATION... ", font=("Avenir",25))
 frst_label.pack(padx=30, pady=30)
 
 scnd_label = tk.Label(top_2,text="Full Name:")
-scnd_label.pack(padx=5, pady=40)
+scnd_label.pack(padx=1, pady=1)
+
+scnd_entry = tk.Entry(top_2)
+scnd_entry.pack(padx=1, pady=2)
 
 thrd_label = tk.Label(top_2,text="Age:")
-thrd_label.pack(padx=5, pady=41)
+thrd_label.pack(padx=1, pady=4)
+
+thrd_entry = tk.Entry(top_2)
+thrd_entry.pack(padx=1, pady=6)
 
 frth_label = tk.Label(top_2,text="Your Contact Number:")
-frth_label.pack(padx=5, pady=42)
+frth_label.pack(padx=1, pady=8)
+
+frth_entry = tk.Entry(top_2)
+frth_entry.pack(padx=1, pady=10)
 
 ffth_label = tk.Label(top_2,text="Person to contact in case of emergency:")
-ffth_label.pack(padx=5, pady=43)
+ffth_label.pack(padx=1, pady=12)
+
+ffth_entry = tk.Entry(top_2)
+ffth_entry.pack(padx=1, pady=14)
 
 sxth_label = tk.Label(top_2,text="His/Her Contact Number:")
-sxth_label.pack(padx=5, pady=44)
+sxth_label.pack(padx=1, pady=16)
+
+sxth_entry = tk.Entry(top_2)
+sxth_entry.pack(padx=1, pady=18)
 
 svth_label = tk.Label(top_2,text="Are you experiencing any symptoms of COVID-19 for the past 7 days?(Y/N):")
-svth_label.pack(padx=5, pady=45)
+svth_label.pack(padx=1, pady=20)
+
+svth_entry = tk.Entry(top_2)
+svth_entry.pack(padx=1, pady=22)
 
 sec_btn1 = tk.Button (top_2,text="Submit Entry")                 
 sec_btn1.pack(padx=65, pady=65)
