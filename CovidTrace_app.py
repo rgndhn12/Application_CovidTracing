e2e29c52d555cd370436a254715d30f750812234
Rgn <reginefaemdahan@iskolarngbayan.pup.edu.ph>
2023-07-19 16:09:58 +0800

    added bg img and color

--- CovidTrace_app.py
+++ CovidTrace_app.py
@@ -9,17 +9,24 @@
 import tkinter as tk
 
 #window
+
 top = tk.Tk()
 top.geometry("1000x1000")
 top.title (" COVID TRACING APP ")
+bg_img = tk.PhotoImage(file="C:\\Users\\USER\\Documents\\OOP Second Sem\\CTracing_App\\imgbg.png")
+tk.Label(top, image=bg_img).pack()
+top['background'] = 'black'
 
-main_label = tk.Label(top, text= " HEALTH MATTERS ", font=("Avenir",25)) 
+main_label = tk.Label(top, text= " HEALTH MATTERS ", font=("Arial",40),
+                      bg="black",fg="Pink")
 main_label.pack(padx=30, pady=30)
 
-main_btn1 = tk.Button (top, text="Add Entry")
-main_btn1.pack(padx=100, pady=100)
+
+main_btn1 = tk.Button (top,text="Add Entry")                  
+main_btn1.pack(padx=50, pady=50)
+
 
 main_btn2 = tk.Button (top, text="Search Entry")
-main_btn2.pack(padx=300, pady=300)
+main_btn2.pack(padx=100, pady=100)
 
 top.mainloop()
--- Info_window.py
+++ Info_window.py
@@ -4,10 +4,24 @@ import tkinter as tk
 
 #window
 top = tk.Tk()
-top.geometry("1000x1000")
+top.geometry("500x500")
 top.title (" COVID TRACING APP ")
 
 frst_label = tk.Label(top, text= " Your Information... ", font=("Avenir",25)) 
-frst_label.pack(padx=30, pady=30)
+frst_label.grid(row=2, column=3)
+
+scnd_label = tk.Label(top,text="Full Name:")
+scnd_label.grid(row=500, column=3)
+
+thrd_label = tk.Label(top,text="Your Contact Number:")
+thrd_label.grid(row=502, column=3)
+
+
+
+frth_label = tk.Label(top,text="Person to call in case of emergency:")
+frth_label.grid(row=504, column=3)
+
+ffth_label = tk.Label(top,text="His/Her Contact Number:")
+ffth_label.grid(row=506, column=3)
 
 top.mainloop()
