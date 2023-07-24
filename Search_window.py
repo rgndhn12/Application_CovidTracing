86e881545fc563be2cb40c95488bf1894c5b2c10
Rgn <reginefaemdahan@iskolarngbayan.pup.edu.ph>
2023-07-24 20:07:25 +0800

    added textbox and button to start searching entry

--- Search_window.py
+++ Search_window.py
@@ -6,12 +6,19 @@ class SearchWindow:
         print(master)
         self.__window = Toplevel(self.__master)
         self.__frame = Frame(self.__window)
-        self.__frame.pack(padx=500, pady=500)
+        self.__frame.pack(padx=100, pady=100)
 
         self.make_form()
+
     def make_form(self):
         search_label = Label(self.__frame, text="Enter the submitted Name:")
         search_label.pack
+
+        self.__search_entry = Entry(self.__frame)
+        self.__search_entry.pack()
+
+        search_btn = Button(self.__frame, text="Search", command=self.__search_entry)
+        search_btn.pack()
 
 
 
