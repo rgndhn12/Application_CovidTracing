fdba1910215cc5a6630ad2063a1af4eb563c4c89
Rgn <reginefaemdahan@iskolarngbayan.pup.edu.ph>
2023-07-24 20:02:07 +0800

    made a function for form

--- Search_window.py
+++ Search_window.py
@@ -8,4 +8,12 @@ class SearchWindow:
         self.__frame = Frame(self.__window)
         self.__frame.pack(padx=500, pady=500)
 
+        self.make_form()
+    def make_form(self):
+        search_label = Label(self.__frame, text="Enter the submitted Name:")
+        search_label.pack
+
+
+
+        
 
